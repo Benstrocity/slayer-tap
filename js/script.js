bf1db87cdd84baf2910717b198c389203ea0f0b6
Benstrocity <byoungblood.c@gmail.com>
2018-11-28 07:23:06 -0600

    initial setup for game

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const enemyField = document.querySelector('#enemyField');
+const upgrades;
+const restart;
+
+let totalDamage = 0;
+let totalHits = 0;
+let enemyHealth = 100/100;
+let level = 1;
+let exp = 0;
+let sp = 0;
