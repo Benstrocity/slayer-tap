2990d1acc01e2d30e8f37af84c520fec3ae433c9
Ben Youngblood <byoungblood@CRHS.local>
2018-11-29 15:16:18 -0600

    refactor functions to arrow functions

--- js/script.js
+++ js/script.js
@@ -1,24 +1,14 @@
 const enemyField = document.querySelector('#enemyField');
 const upgrades = document.querySelector('#upgMenu');
 const restart = document.querySelector('#restartGame');
-let totalDmgSpan = document.querySelector('#totalDmg');
-let totalHitsSpan = document.querySelector('#totalHits');
-let enemyNameH3 = document.querySelector('#enemyName');
-let enemyHealthP = document.querySelector('#enemyHealth');
-let gameLvlSpan = document.querySelector('#gameLvl');
-let plyrExpSpan = document.querySelector('#plyrExp');
-let remExpSpan = document.querySelector('#remExp');
-let spSpan = document.querySelector('#sp');
-let totalDamage = 0;
-let totalHits = 0;
-let hitPower = 10;
-let enemyName;
-let enemyHealth;
-let enemyHitPoints;
-let level = 1;
-let exp = 0;
-let remExp = 100;
-let sp = 0;
+const totalDmgSpan = document.querySelector('#totalDmg');
+const totalHitsSpan = document.querySelector('#totalHits');
+const enemyNameH3 = document.querySelector('#enemyName');
+const enemyHealthP = document.querySelector('#enemyHealth');
+const gameLvlSpan = document.querySelector('#gameLvl');
+const plyrExpSpan = document.querySelector('#plyrExp');
+const remExpSpan = document.querySelector('#remExp');
+const spSpan = document.querySelector('#sp');
 const enemies = {
     enemy1: {
         name: 'Giant Worm',
@@ -33,32 +23,37 @@ const enemies = {
         hitPoints: 150
     }
 };
+let totalDamage = 0;
+let totalHits = 0;
+let hitPower = 10;
+let enemyName;
+let newHealth;
+let enemyHealth;
+let enemyHitPoints;
+let level = 1;
+let exp = 0;
+let remExp = 100;
+let sp = 0;
 
-function createEnemy() {
+const createEnemy = () => {
     let randNum = Math.floor(Math.random() * 3) + 1;
     if (randNum === 1) {
         enemyName = enemies.enemy1.name;
         enemyHitPoints = enemies.enemy1.hitPoints;
+        enemyHealth = enemyHitPoints + ' / ' + enemyHitPoints;
     } else if (randNum === 2) {
         enemyName = enemies.enemy2.name;
         enemyHitPoints = enemies.enemy2.hitPoints;
+        enemyHealth = enemyHitPoints + ' / ' + enemyHitPoints;
     } else if (randNum === 3) {
         enemyName = enemies.enemy3.name;
         enemyHitPoints = enemies.enemy3.hitPoints;
+        enemyHealth = enemyHitPoints + ' / ' + enemyHitPoints;
     }
 }
-
-function attackEnemy() {
-    enemyHealth = enemyHitPoints + ' / ' + enemyHitPoints;
-    enemyField.addEventListener('click', () => {
-        enemyHealth = enemyHitPoints - hitPower;
-        enemyHealthP.innerHTML = enemyHealth + ' / ' + enemyHitPoints;
-    });
-}
     
-function newGame() {
+const newGame = () => {
     createEnemy();
-    attackEnemy();
     
     totalDmgSpan.innerHTML = totalDamage;
     totalHitsSpan.innerHTML = totalHits;
@@ -66,8 +61,14 @@ function newGame() {
     enemyHealthP.innerHTML = enemyHealth;
     gameLvlSpan.innerHTML = level;
     plyrExpSpan.innerHTML = exp;
+    remExpSpan.innerHTML = remExp;
     spSpan.innerHTML = sp;
 }
+
+enemyField.addEventListener('click', () => {
+    newHealth = enemyHitPoints - hitPower;
+    enemyHealthP.innerHTML = newHealth + ' / ' + enemyHitPoints;
+});
 
 newGame();
 
