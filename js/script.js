f926c64a0995f0181a29bdb68cab022609a438ca
Benstrocity <byoungblood.c@gmail.com>
2018-11-29 20:32:10 -0600

    combat and leveling system added

--- js/script.js
+++ js/script.js
@@ -1,73 +1,153 @@
 const enemyField = document.querySelector('#enemyField');
 const upgrades = document.querySelector('#upgMenu');
 const restart = document.querySelector('#restartGame');
-let totalDmgSpan = document.querySelector('#totalDmg');
-let totalHitsSpan = document.querySelector('#totalHits');
-let enemyNameH3 = document.querySelector('#enemyName');
-let enemyHealthP = document.querySelector('#enemyHealth');
-let gameLvlSpan = document.querySelector('#gameLvl');
-let plyrExpSpan = document.querySelector('#plyrExp');
-let remExpSpan = document.querySelector('#remExp');
-let spSpan = document.querySelector('#sp');
-let totalDamage = 0;
-let totalHits = 0;
-let hitPower = 10;
+const totalDmgSpan = document.querySelector('#totalDmg');
+const totalHitsSpan = document.querySelector('#totalHits');
+const enemyNameH3 = document.querySelector('#enemyName');
+const enemyHealthP = document.querySelector('#enemyHealth');
+const gameLvlSpan = document.querySelector('#gameLvl');
+const plyrExpSpan = document.querySelector('#plyrExp');
+const remExpSpan = document.querySelector('#remExp');
+const spSpan = document.querySelector('#sp');
+const enemies = 
+      [
+          ['Mutated Rat', 'Rabbid Dog', 'Giant Worm'],
+          [250, 500, 750]
+      ];
 let enemyName;
 let enemyHealth;
 let enemyHitPoints;
+let totalDamage = 0;
+let hitPower = 10;
+let totalHits = 0;
 let level = 1;
 let exp = 0;
 let remExp = 100;
 let sp = 0;
-const enemies = {
-    enemy1: {
-        name: 'Giant Worm',
-        hitPoints: 100
-    },
-    enemy2: {
-        name: 'Mutated Rat',
-        hitPoints: 125
-    },
-    enemy3: {
-        name: 'Rabbid Dog',
-        hitPoints: 150
-    }
-};
 
-function createEnemy() {
-    let randNum = Math.floor(Math.random() * 3) + 1;
-    if (randNum === 1) {
-        enemyName = enemies.enemy1.name;
-        enemyHitPoints = enemies.enemy1.hitPoints;
-    } else if (randNum === 2) {
-        enemyName = enemies.enemy2.name;
-        enemyHitPoints = enemies.enemy2.hitPoints;
-    } else if (randNum === 3) {
-        enemyName = enemies.enemy3.name;
-        enemyHitPoints = enemies.enemy3.hitPoints;
-    }
-}
-
-function attackEnemy() {
-    enemyHealth = enemyHitPoints + ' / ' + enemyHitPoints;
-    enemyField.addEventListener('click', () => {
-        enemyHealth = enemyHitPoints - hitPower;
-        enemyHealthP.innerHTML = enemyHealth + ' / ' + enemyHitPoints;
-    });
-}
-    
-function newGame() {
-    createEnemy();
-    attackEnemy();
-    
-    totalDmgSpan.innerHTML = totalDamage;
-    totalHitsSpan.innerHTML = totalHits;
-    enemyNameH3.innerHTML = enemyName;
-    enemyHealthP.innerHTML = enemyHealth;
+//Load default stat and menu values
+const loadDefaults = () => {
     gameLvlSpan.innerHTML = level;
     plyrExpSpan.innerHTML = exp;
-    spSpan.innerHTML = sp;
+    remExpSpan.innerHTML = remExp;
+}
+
+//Creates a random enemy
+const createEnemy = () => {
+    let randNum = Math.floor(Math.random() * 3) + 1;
+    for (let i = 0; i < randNum; i++) {
+        enemyName = enemies[0][i];
+        enemyHitPoints = enemies[1][i];
+    }
+    enemyNameH3.innerHTML = enemyName;
+    enemyHealthP.innerHTML = enemyHitPoints;
+}
+
+//Attack behavior
+const attack = () => {
+    enemyField.addEventListener('click', () => {
+        enemyHitPoints = enemyHitPoints - hitPower;
+        enemyHealthP.innerHTML = enemyHitPoints;
+        if (enemyHitPoints <= 0) {
+            killEnemy();
+        }
+    });
+}
+
+//Enemy death behavior
+const killEnemy = () => {
+    createEnemy();
+    updateLevel();
+}
+
+//Update experience and level
+const updateLevel = () => {
+    plyrExpSpan.innerHTML = exp;
+    gameLvlSpan.innerHTML = level;
+    remExpSpan.innerHTML = remExp;
+    exp += 50;
+    if (exp > 0 && exp < 100) {
+        level = 1;
+        remExp = 100 - exp;
+    } else if (exp > 99 && exp < 500) {
+        level = 2;
+        hitPower += 25;
+        remExp = 500 - exp;
+    } else if (exp > 499 && exp < 1000) {
+        level = 3;
+        hitPower += 25;
+        remExp = 1000 - exp;
+    } else if (exp > 999 && exp < 1750) {
+        level = 4;
+        hitPower += 25;
+        remExp = 1750 - exp;
+    } else if (exp > 1749 && exp < 2500) {
+        level = 5;
+        hitPower += 25;
+        remExp = 2500 - exp;
+    } else if (exp > 2499 && exp < 4000) {
+        level = 6;
+        hitPower += 25;
+        remExp = 4000 - exp;
+    } else if (exp > 3999 && exp < 6500) {
+        level = 7;
+        hitPower += 25;
+        remExp = 6500 - exp;
+    } else if (exp > 6499 && exp < 10000) {
+        level = 8;
+        hitPower; //Max
+        remExp = 10000 - exp;
+    } else if (exp > 9999 && exp < 20000) {
+        level = 9;
+        hitPower; //Max
+        remExp = 20000 - exp;
+    } else if (exp > 19999 && exp < 35000) {
+        level = 10;
+        hitPower; //Max
+        remExp = 35000 - exp;
+    } else {
+        maxLevel();
+    }
+}
+
+const maxLevel = () => {
+    if (exp === 4000) {
+        enemyField.innerHTML = 
+            `<h2>Congratulations, you have reached the max level!</h2>`;
+        enemyField.addEventListener('click', () => {
+            alert(`Click 'Restart Game' to play again.`)
+        });
+    }
+}
+
+//Upgrades menu
+const upgrade = () => {
+    upgrades.addEventListener('click', () => {
+        alert('Feature coming soon.');
+    });
+}
+
+//Restart the game
+const reset = () => {
+    restart.addEventListener('click', () => {
+        location.reload();
+    });
+}
+
+//Launch a new game
+const newGame = () => {
+    loadDefaults();
+    createEnemy();
+    attack();
+    updateLevel();
+    upgrade();
+    reset();
 }
 
 newGame();
+
+
+
+
+
 
