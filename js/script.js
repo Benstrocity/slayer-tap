cb073b0f1236818b31772466214dbf7037891bb6
Benstrocity <byoungblood.c@gmail.com>
2018-12-04 06:17:21 -0600

    see changelog

--- js/script.js
+++ js/script.js
@@ -10,6 +10,8 @@ const gameLvlSpan = document.querySelector('#gameLvl');
 const plyrExpSpan = document.querySelector('#plyrExp');
 const remExpSpan = document.querySelector('#remExp');
 const spSpan = document.querySelector('#sp');
+const dmgBtn = document.querySelector('#dmgBtn');
+const spBtn = document.querySelector('#spBtn');
 const enemies = 
       [
           ['Mutated Rat', 'Rabbid Dog', 'Giant Worm'],
@@ -33,18 +35,17 @@ const createEnemy = () => {
         enemyName = enemies[0][i];
         enemyHitPoints = enemies[1][i];
     }
-    enemyNameH3.innerHTML = enemyName;
-    enemyHealthP.innerHTML = enemyHitPoints;
 }
 
 //Attack behavior
 const attack = () => {
     enemyField.addEventListener('click', () => {
         enemyHitPoints = enemyHitPoints - hitPower;
-        enemyHealthP.innerHTML = enemyHitPoints;
+        updateCounters();
         if (enemyHitPoints <= 0) {
             killEnemy();
             updateLevel();
+            earnSP(25); //Earn slayerpoints
         }
         damageDealt();
         totalClicks();
@@ -53,114 +54,78 @@ const attack = () => {
 
 //Enemy death behavior
 const killEnemy = () => {
-    spSpan.innerHTML = sp;
     createEnemy();
-    let earnSP = () => {
-        spSpan.innerHTML = sp;
-        sp += 25;
-    }
-    earnSP(); //Earn slayerpoints
 }
 
 
 //Tally up total damage dealt
 const damageDealt = () => {
-    totalDmgSpan.innerHTML = totalDamage;
     totalDamage += hitPower;
+    updateCounters();
 }
 
 //Tally up total amount of clicks/taps on enemy
 const totalClicks = () => {
-    spSpan.innerHTML = sp;
-    totalHitsSpan.innerHTML = totalHits;
     totalHits += 1;
-    let earnSP = () => {
-        sp += 2;
-    }
-    earnSP(); //Earn slayerpoints
+    earnSP(2); //Earn slayerpoints
+    updateCounters();
 }
+
+//Add SP
+const earnSP = addSP => sp += addSP;
 
 //Update experience and level
 const updateLevel = () => {
-    plyrExpSpan.innerHTML = exp;
-    gameLvlSpan.innerHTML = level;
-    remExpSpan.innerHTML = remExp;
     exp += 50;
     if (exp > 0 && exp < 100) {
         level = 1;
         remExp = 100 - exp;
+        earnSP(0);
     } else if (exp > 99 && exp < 250) {
         level = 2;
         hitPower += 1;
         remExp = 500 - exp;
-        let earnSP = () => {
-            sp += 50;
-        }
-    earnSP(); //Earn slayerpoints   
+        earnSP(50); 
     } else if (exp > 249 && exp < 500) {
         level = 3;
         hitPower += 1;
         remExp = 1000 - exp;
-        let earnSP = () => {
-            sp += 50;
-        }
-    earnSP(); //Earn slayerpoints
+        earnSP(50);
     } else if (exp > 499 && exp < 850) {
         level = 4;
         hitPower += 1;
         remExp = 1750 - exp;
-        let earnSP = () => {
-            sp += 50;
-        }
-    earnSP(); //Earn slayerpoints
+        earnSP(50);
     } else if (exp > 849 && exp < 1200) {
         level = 5;
         hitPower += 1;
         remExp = 2500 - exp;
-        let earnSP = () => {
-            sp += 50;
-        }
-    earnSP();
+        earnSP(50);
     } else if (exp > 1199 && exp < 1600) {
         level = 6;
         hitPower += 1;
         remExp = 4000 - exp;
-        let earnSP = () => {
-            sp += 50;
-        }
-    earnSP(); //Earn slayerpoints
+        earnSP(50);
     } else if (exp > 1799 && exp < 2200) {
         level = 7;
         hitPower; //Max power from leveling
         remExp = 6500 - exp;
-        let earnSP = () => {
-            sp += 50;
-        }
-    earnSP(); //Earn slayerpoints
+        earnSP(50);
     } else if (exp > 2199 && exp < 3000) {
         level = 8;
         hitPower += 1;
         remExp = 10000 - exp;
-        let earnSP = () => {
-            sp += 50;
-        }
-    earnSP(); //Earn slayerpoints
+        earnSP(50);
     } else if (exp > 2999 && exp < 5000) {
         level = 9;
         hitPower += 1;
         remExp = 20000 - exp;
-        let earnSP = () => {
-            sp += 50;
-        }
-    earnSP(); //Earn slayerpoints
+        earnSP(50);
     } else if (exp > 4999 && exp < 10000) {
         level = 10;
         hitPower += 1;
         remExp = 35000 - exp;
-        let earnSP = () => {
-            sp += 50;
-        }
-    earnSP(); //Earn slayerpoints
+        earnSP(50);
     } else {
         maxLevel();
     }
@@ -177,9 +142,65 @@ const maxLevel = () => {
     }
 }
 
-//Click behavior for Upgrades menu
-const toggleMenu = () => {
+//Behavior of buttons when clicked
+const btnAccept = selectBtn => {
+    selectBtn.addEventListener('mousedown', () => {
+        selectBtn.style.color = '#0f0';
+    });
+    selectBtn.addEventListener('mouseup', () => {
+        selectBtn.style.color = '#000'; 
+    });
+}
+
+const btnDeny = selectBtn => {
+    selectBtn.addEventListener('mousedown', () => {
+        selectBtn.style.color = '#f00';
+    });
+    selectBtn.addEventListener('mouseup', () => {
+        selectBtn.style.color = '#000'; 
+    });
+}
+
+//Purchasing upgrades
+const buyUpgrades = () => {
+    //Prevents btnAccept/btnDeny delay on first click
+    btnAccept(dmgBtn);
+    btnAccept(spBtn);
+    btnDeny(dmgBtn);
+    btnDeny(spBtn);
+    spBtn.addEventListener('click', () => {
+        if (sp >= 500) {
+            sp -= 500;
+            hitPower += 10; 
+            updateCounters();
+            btnAccept(spBtn);
+        } else {
+            btnDeny(spBtn);
+        }
+    });
     
+    dmgBtn.addEventListener('click', () => {
+        if (sp >= 500) {
+            sp -= 500;
+            updateCounters();
+            btnAccept(dmgBtn)
+        } else {
+            btnDeny(dmgBtn);
+        }
+    });
+}
+
+//Function to update counters when other actions occur
+const updateCounters = () => {
+    totalDmgSpan.innerHTML = totalDamage;
+    totalHitsSpan.innerHTML = totalHits;
+    spSpan.innerHTML = sp;
+    enemyHealthP.innerHTML = enemyHitPoints;
+    enemyNameH3.innerHTML = enemyName;
+    enemyHealthP.innerHTML = enemyHitPoints;
+    plyrExpSpan.innerHTML = exp;
+    gameLvlSpan.innerHTML = level;
+    remExpSpan.innerHTML = remExp;
 }
 
 //Restart the game
@@ -191,15 +212,14 @@ const reset = () => {
 
 //Launch a new game
 const newGame = () => {
-    createEnemy();
-    attack();
-    updateLevel();
-    damageDealt();
-    totalClicks();
-    reset();
+    createEnemy(); //Spawn the first enemy
+    attack(); //Enable attack functionality
+    buyUpgrades(); //Enable the shop to be used
+    reset(); //Allow new games to be started
+    updateCounters(); //Set counters to load defaults
 }
 
-newGame();
+newGame(); //Start a new game on load
 
 
 
